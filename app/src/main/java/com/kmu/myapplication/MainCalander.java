7f4fad06c57dfc3f276db1ad97ed6489c13e154c
anheew1 <anheew1@kookmin.ac.kr>
2018-11-08 20:37:17 +0900

    make ViewPager(not work)

--- app/src/main/java/com/kmu/myapplication/MainCalander.java
+++ app/src/main/java/com/kmu/myapplication/MainCalander.java
@@ -1,30 +1,23 @@
 package com.kmu.myapplication;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.TextView;
-import android.widget.BaseAdapter;
-
 
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.List;
 import java.util.Locale;
-import java.util.zip.Inflater;
 
 public class MainCalander extends AppCompatActivity {
+    private int COUNT = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +30,6 @@ public class MainCalander extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_dehaze_black_18dp);
         getSupportActionBar().setTitle("");
 
-        GridView mainCalender = findViewById(R.id.calendar);
         ArrayList<String> weekdayList = new ArrayList<String>();
         weekdayList.add("일");
         weekdayList.add("월");
@@ -47,124 +39,40 @@ public class MainCalander extends AppCompatActivity {
         weekdayList.add("금");
         weekdayList.add("토");
 
-        ArrayList<String> dayList = new ArrayList<String>();
+        // Calender is add by FragCalander
 
-        Calendar cal = new GregorianCalendar(Locale.KOREA);
-        int startDay = cal.get(Calendar.DAY_OF_WEEK); // 월 시작 요
-        int lastDay = cal.getActualMaximum(Calendar.DATE); // 월 마지막 날짜
-
-        TextView ymdate = (TextView) findViewById(R.id.ymdate);
-        String symdate=cal.get(Calendar.YEAR)+ "년 " +(cal.get(Calendar.MONTH)+1)+"월";
-        ymdate.setText(symdate);
-
-        for (int i = 0; i <= startDay; i++) {
-            dayList.add(" ");
-        }
-        for (int i = 1; i <= lastDay; i++) {
-            String day = String.valueOf(i);
-            dayList.add(day);
-        }
-        for(int i=0;i <((lastDay+startDay)/7) - 3;i++){
-            dayList.add(" ");
-        }
-        CalenderAdapter calenderAdapter = new CalenderAdapter(getApplicationContext(),dayList);
-        mainCalender.setAdapter(calenderAdapter);
         GridView weekday = findViewById(R.id.weekday);
         WeekdayAdapter weekdayAdapter = new WeekdayAdapter(getApplicationContext(),weekdayList);
         weekday.setAdapter(weekdayAdapter);
 
+        final ViewPager viewPager= (ViewPager) findViewById(R.id.viewpager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setCurrentItem(1);
+        viewPager.setAdapter(viewPagerAdapter);
 
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position < COUNT){
+                    viewPager.setCurrentItem(position+COUNT,false);
+                }
+                else if (position >= COUNT*2){
+                    viewPager.setCurrentItem(position-COUNT,false);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
     }
-    public class CalenderAdapter extends BaseAdapter{
-        private List list;
-        private Context context;
-        private LayoutInflater inflater;
-        public CalenderAdapter(Context context, List<String> list) {
-            this.list =list;
-            this.context = context;
-            this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
 
-        @Override
-        public int getCount() {
-            return list.size();
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return list.get(position);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-
-            if(convertView == null){
-                convertView = inflater.inflate(R.layout.day,parent,false);
-                holder = new ViewHolder();
-                holder.text = (TextView)convertView.findViewById(R.id.daylayout);
-                convertView.setTag(holder);
-            }
-            else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-            holder.text.setText(String.valueOf(getItem(position)));
-
-            if(position%7==0){
-                holder.text.setTextColor(Color.RED);
-            }
-            else if(position%7 == 6){
-                holder.text.setTextColor(Color.BLUE);
-            }
-            else {
-                holder.text.setTextColor(Color.GRAY);
-            }
-            Calendar calendar = new GregorianCalendar(Locale.KOREA);
-            Integer today = calendar.get(Calendar.DAY_OF_MONTH);
-            String sToday = String.valueOf(today);
-            if (sToday.equals(getItem(position))) { //오늘 day 텍스트 컬러 변경
-                holder.text.setTextColor(Color.BLACK);
-            }
-            return convertView;
-}
-    }
-    protected class WeekdayAdapter extends CalenderAdapter{
-        private List list;
-        private Context context;
-        private LayoutInflater inflater;
-        public WeekdayAdapter(Context context, List<String> list) {
-            super(context,list);
-            this.list =list;
-            this.context = context;
-            this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-
-            if(convertView == null){
-                convertView = inflater.inflate(R.layout.weekday,parent,false);
-                holder = new ViewHolder();
-                holder.text = (TextView)convertView.findViewById(R.id.weekdaylayout);
-                convertView.setTag(holder);
-            }
-            else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-            holder.text.setText(String.valueOf(getItem(position)));
-
-            return convertView;
-        }
-    }
-private class ViewHolder{
-    TextView text;
-}
 
 
 
---
+++ app/src/main/java/com/kmu/myapplication/ViewPagerAdapter.java
@@ -0,0 +1,26 @@
+package com.kmu.myapplication;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    private static int COUNT = 100;
+    private ArrayList<FragCalander> fragcalanderList;
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+    @Override
+    public int getCount() {
+        return COUNT*3;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return FragCalander.newInstance(i%COUNT);
+    }
+}
