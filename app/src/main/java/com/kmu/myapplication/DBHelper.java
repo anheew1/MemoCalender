cc69210a9ff423d6f483c9bece5276497efc50c0
anheew1 <anheew1@kookmin.ac.kr>
2018-12-16 02:17:16 +0900

    Create DBHelper.java

---
+++ app/src/main/java/com/kmu/myapplication/DBHelper.java
@@ -0,0 +1,27 @@
+package com.kmu.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "events.db";
+    private static final int DATABASE_VERSION = 2;
+
+    public DBHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //reset code
+
+       db.execSQL("CREATE TABLE IF NOT EXISTS events ( id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT, date TEXT, memo TEXT);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS events");
+        onCreate(db);
+    }
+}
